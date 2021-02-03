4b9c8926b0cd58ab949d7d093e06e525a134e1b8
martuza khadri badiger <68277084+martuzakhadri@users.noreply.github.com>
2021-02-03 03:00:08 -0800

    Add files via upload

---
+++ website/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $nav=$('.nav');
+    $togglecollapse=$('.toggle-collapse');
+
+/** click event on toggle**/
+
+$togglecollapse.click(function(){
+
+    $nav.toggleClass('collapse');
+})
+
+//owl carousel for blog
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:false,
+    autoplayTimeout:3000,
+    dots:false,
+    nav:true,
+    navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+});
+
+//click to scroll top
+$('.move-up span').click(function(){
+    $('html,body').animate({
+        scrollTop:0
+    },1000);
+})
+// aos instance
+AOS.init();
+
+
+
+});
